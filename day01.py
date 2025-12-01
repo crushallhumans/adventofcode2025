6f2e95468f7e8c5107fbe223b23299d5327398c6
Conrad Rushing <conrad.rushing@gmail.com>
2025-12-01 14:07:15 -0500

    Day 1 complete!

--- day01.py
+++ day01.py
@@ -21,11 +21,19 @@ pp = pprint.PrettyPrinter()
 
 ADVENT_YEAR = '2025'
 DEBUG = False
-TEST_INPUT_STRING_ONE = """
-"""
+TEST_INPUT_STRING_ONE = """L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82"""
 TEST_INPUT_STRING_TWO = TEST_INPUT_STRING_ONE
-TEST_ONE_RESULT = 8888
-TEST_TWO_RESULT = 7777
+TEST_ONE_RESULT = 3
+TEST_TWO_RESULT = 6
 
 
 def reprocess_input(param_set):
@@ -39,21 +47,95 @@ def reprocess_input(param_set):
 def one_star(param_set, is_two_star = False):
     if not is_two_star: print("---------------one_star--------------------")
     param_set = reprocess_input(param_set)
-    c = 8888
+    c = 0
+    d = 0
+    dial = 50
+    zero_start = False
+    z = 0
     for i in param_set:
-        continue
+        P(z,i)
+        move = int(i[1:])
+        if i[0] == 'L':
+            dial -= move
+        elif i[0] == 'R':
+            dial += move
+        
+        P(dial)
+        passed_thru = False
+
+        dd = 0
+        if move > 99:
+            P('passes > 1')
+
+        if dial > 99:
+            passed_thru = True
+            while dial > 99:
+                dd += 1
+                dial -= 100
+        elif dial < 0:
+            passed_thru = True
+            while dial < 0:
+                dd += 1
+                dial += 100
+        P(dial)
+
+        if passed_thru:
+            P('passed_thru')        
+        if zero_start:
+            P('zero_start')
+
+        if zero_start and passed_thru:
+            dd -= 1
+
+        if dial == 0:
+            P('points_at_zero')
+            zero_start = True
+            c += 1
+            if is_two_star:
+                c += (dd-1 if dd > 0 else 0)
+        else:
+            zero_start = False
+            if is_two_star:
+                c += dd
+        z += 1
+
+        P(c)
+#        P(d)
+        P("")
     return c
 
 def two_star(param_set):
     print("---------------two_star--------------------")
-    #return one_star(param_set,True)
+
     param_set = reprocess_input(param_set)
-    c = 7777
+    c = 0
+    dial = 50
+    z = 0
     for i in param_set:
-        continue
+        P(z,i)
+        move = int(i[1:])
+        dir = 1
+        if i[0] == 'L':
+            dir = -1
+
+        x = 0
+        while x < move:
+            dial += dir
+            P(dial)
+            if dial == 0:
+                P("\tzero!")
+                c += 1
+            elif dial > 99:
+                P("\tzero!")
+                c += 1
+                dial -= 100
+            elif dial < -99:
+                P("\tzero!")
+                c += 1
+                dial += 100
+            x += 1
+
     return c
-
-
 
 
 
@@ -118,7 +200,7 @@ if __name__ == '__main__':
 
         start = (time.time() * 1000)
         ret = one_star(input_set)
-        print (ret)
+        #print (ret)
         print ('elapsed:',(time.time() * 1000) - start,'ms')
 
         start = (time.time() * 1000)
