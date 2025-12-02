91a9c331d7f1c23069966a3181458fb1919db632
Conrad Rushing <conrad.rushing@gmail.com>
2025-12-02 13:45:01 -0500

    Day 2 complete

--- day02.py
+++ day02.py
@@ -21,32 +21,96 @@ pp = pprint.PrettyPrinter()
 
 ADVENT_YEAR = '2025'
 DEBUG = False
-TEST_INPUT_STRING_ONE = """
-"""
+TEST_INPUT_STRING_ONE = """11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124"""
 TEST_INPUT_STRING_TWO = TEST_INPUT_STRING_ONE
-TEST_ONE_RESULT = 8888
-TEST_TWO_RESULT = 7777
+TEST_ONE_RESULT = 1227775554
+TEST_TWO_RESULT = 4174379265
 
 
 def reprocess_input(param_set):
     if isinstance(param_set,str):
         l = []
         l = [input_line.strip() for input_line in param_set.splitlines()]
-        param_set = l
+        param_set = l[0].split(',')
     return param_set    
 
+def repeater_check(i):
+    s = str(i)
+    l = len(s)
+    if l % 2:
+        return False
+    d = int(l/2)
+    a = s[0:d]
+    b = s[d:]
+    if (d == 1):
+        a = s[0]
+        b = s[1]
+    if a == b:    
+        P(i)
+        P(d)
+        P(a)
+        P(b)
+        P('repeater - invalid')        
+        return True
+    return 0
+
+def repeater_check_two(i):
+    s = str(i)
+    # INCORRECT - did not properly ignore single-item sets
+    # if len(set(list(s))) == 1:
+    #     P(i)
+    #     P([int(m) for m in list(s)])
+    #     P('repeater at layer 1 - invalid')        
+    #     return True
+    l = len(s)
+    d = l//2
+    while d > 0:
+    # while d > 1: # skip 1 - handled by set comprehension above (except incorrect)
+        if l % d:
+            d -= 1
+            continue
+        layer_step = 0
+        comparison_list = []
+        while layer_step < l:
+            a = s[0+layer_step:d+layer_step]
+            if not a[0]:
+                print(s,d,a,l,layer_step)
+                exit()
+            comparison_list.append(int(a))
+            layer_step += d
+        if len(comparison_list) > 1 and len(set(comparison_list)) == 1:
+            P(i)
+            P(comparison_list)
+            P(f'repeater at layer {d} - invalid')    
+            return True
+
+        d -= 1
+
+    return False
 
 def one_star(param_set, is_two_star = False):
     if not is_two_star: print("---------------one_star--------------------")
+    P(param_set)
     param_set = reprocess_input(param_set)
-    c = 8888
+    c = 0
     for i in param_set:
-        continue
+        range = [int(j) for j in i.split('-')]
+        d = range[0]
+        P(range)
+        while d < range[1]+1:
+            x = repeater_check(d) if not is_two_star else repeater_check_two(d)
+            if x:
+                c += d
+                P(c)
+                P('----------------')
+            d += 1
+        P('*****************************')
+        P()
     return c
 
 def two_star(param_set):
     print("---------------two_star--------------------")
-    #return one_star(param_set,True)
+    return one_star(param_set,True)
     param_set = reprocess_input(param_set)
     c = 7777
     for i in param_set:
